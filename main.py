ff86d5ba5e641aaf44fb35ee4272c6ed1dc6d39f
LinusThoennesOtto <linus.thoennes@otto.de>
2025-09-15 15:16:43 +0200

    Plus oder Minus

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ def main():
     x = float(input("Zahl 1: "))
     y = float(input("Zahl 2: "))
 
-    print(f"{x} + {y} = {add(x, y)}")
+    match(input("Rechnung: ").lower()):
+        case "+" | "plus" | "addieren" | "addition":
+            print(f"{x} + {y} = {add(x, y)}")
+        case "-" | "minus" | "subtrieren" | "subtraktion":
+            print(f"{x} . {y} = {sub(x, y)}")
 
 
 def add(x, y):
